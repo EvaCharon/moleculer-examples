054d56bce7c89ddbadec4de34e0fb9ec231ed920
EvaCharon <583946482@qq.com>
2022-10-20 19:47:58 +0800

    reset post.like-2

--- blog/services/posts.service.js
+++ blog/services/posts.service.js
@@ -35,9 +35,10 @@ module.exports = {
 	actions: {
 
 		like(ctx) {
+			
 			const users = await this.broker.call("users.find");
-			let u_id = users.find(u => u.username==ctx.username)._id;
-			let p_id = ctx.p_id;
+			let u_id = users.find(u => u.username==ctx.params.username)._id;
+			let p_id = ctx.params.p_id;
 			const like = await this.broker.call("likes.create",{post:p_id,user:u_id});
 			return like;
 		
