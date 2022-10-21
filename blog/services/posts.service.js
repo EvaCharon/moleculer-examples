165454eaa16a0c6094f451c86e20673b00c0af09
EvaCharon <583946482@qq.com>
2022-10-21 11:33:19 +0800

    add similarity3

--- blog/services/posts.service.js
+++ blog/services/posts.service.js
@@ -74,7 +74,7 @@ module.exports = {
 		
 		},
 		async getSimilarity(ctx){
-				let id = ctx.query.id;
+				let id = ctx.params.id;
 				const allPosts = await thid.adapter.find();
 				const currentPost = allPosts.find(p => p._id==id);
 				let content = currentPost.content;
